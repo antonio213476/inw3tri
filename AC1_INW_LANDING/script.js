6d9784d48453d10130e434c220d5a6c905994c78
antonio21347 <antoniofilisettidicillo@gmail.com>
2024-03-08 15:01:25 -0300

    Progresso?

--- AC1_INW_LANDING/script.js
+++ AC1_INW_LANDING/script.js
@@ -14,13 +14,45 @@ function main(){
     navBar.style.backgroundColor = '#b35729'
     navBar.style.display = 'flex'
     navBar.style.alignItems = 'center'
-    theBody.appendChild(navBar)
     
-    const textNavBar = document.createElement('h2')
-    textNavBar.style.margin = '0'
-    textNavBar.style.marginLeft = '2%'
-    textNavBar.textContent = 'Bienvenido a mi sitio'
-    textNavBar.style.fontWeight = '600'
-    navBar.appendChild(textNavBar)
+    const textBig = document.createElement('h2')
+    textBig.style.margin = '0'
+    textBig.style.marginLeft = '2%'
+    textBig.textContent = 'Bemvindo a meu site'
+    const cloneTextBig = textBig.cloneNode(true)
 
+    const divQuemSou = document.createElement('div')
+    divQuemSou.style.marginTop = '2rem'
+    divQuemSou.style.display = 'flex'
+    divQuemSou.classList.add('quemSou') 
+
+    const tableCoiso = document.createElement('table')
+    tableCoiso.style.width = '100%'
+
+    const tableQuemSou = document.createElement('td')
+    tableQuemSou.classList.add('TdQuemSou')
+    tableQuemSou.style.width = '50%'
+    tableQuemSou.style.alignSelf = 'top'
+    const cloneQuem = tableQuemSou.cloneNode(true)
+
+
+    const textoQuemSou2 = document.createElement('img')
+    textoQuemSou2.style.width = '50%'
+    textoQuemSou2.src = 'https://picsum.photos/200/300'
+
+    theBody.appendChild(navBar)
+    theBody.appendChild(divQuemSou)
+
+    navBar.appendChild(textBig)
+  
+    divQuemSou.appendChild(tableCoiso)
+    tableCoiso.appendChild(tableQuemSou)
+    
+    cloneTextBig.textContent = 'Quem sou'
+    tableQuemSou.appendChild(cloneTextBig)
+    tableCoiso.appendChild(tableQuemSou)
+
+    
+    cloneQuem.appendChild()
+    tableCoiso.appendChild(cloneQuem)
 }
